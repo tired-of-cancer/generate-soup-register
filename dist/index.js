0e4ed70efff3af90986b2cadf45d7cf1679c313d
Rik van Duijn <ik@rikvanduijn.nl>
2022-12-13 11:51:47 +0100

    Add ncc bundle distribution

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+// TODO: Source file
+console.log('hello world');
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
